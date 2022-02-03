8f5f4ffa1dd5c522fd07c609d8524b4997bdb86d
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-02-03 01:22:13 +0100

    Delete otisak directory

--- otisak/s30.cpp
+++
@@ -1,9 +0,0 @@
-/*
-Označiti tačne rečenice:
-
-1. Konstruktor mora da ima isto ime kao klasa.
-2. Klasa može da nasledi više od jedne klase.
-3. Podrazumevani konstruktor je isto što i prazan konstruktor.
-*/
-
-// Tačne su rečenice 1. i 2.
