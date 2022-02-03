9265096c3404b0fd9a2ba76c7d03997e25758075
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-02-03 02:30:55 +0100

    ALO OVO JE ONO ŠTO SAM TRAŽILA

---
+++ otisak/kodovi/s4.cpp
@@ -0,0 +1,29 @@
+// Šta će biti ispisano posle izvršavanja sledećeg koda?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, z;
+	int *px, *py, *pz;
+
+	x = 1;
+	y = 2;
+	z = 3;
+
+	px = &x;
+	py = &y;
+	pz = &z;
+
+	// dafuq
+	int &rx = *px, ry = *py, &rz = *pz;
+
+	*py = ry;		// ry = &y  ->  *py = ry = &y  =>  *py = &y (py dobija adresu od y i postaje pokazivač na y)
+	*pz = y;		// na adresu pz se upisuje vrednot iz y
+	x = *py;		// x preuzima vrednost iz lokacije na koju py pokazuje (preuzima vrednost y)
+
+	cout << z << x << y << "\n";		// kako je y = 2, ispisaće se 222
+	return 0;
+}
+
+// ISPIS: 222
