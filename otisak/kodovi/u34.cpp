2610561f9986c860b6b76339ea3bf0639d1ddef2
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-02-03 03:00:31 +0100

    Create u34.cpp

---
+++ otisak/kodovi/u34.cpp
@@ -0,0 +1,33 @@
+// Šta će biti ispisano posle izvršavanja sledećeg koda?
+
+#include<iostream>
+using namespace std;
+
+class A {
+	protected:
+		int x, y;
+	protected:
+		A(int xx = 1, int yy = 1) : x(xx), y(yy) {}
+		int pomnozi() {return x * y;}
+		int saberi() {return x + y;}
+};
+
+class B : public A {
+	private:
+		int z;
+	public:
+		B(int xx = 1, int yy = 2, int zz = 3) : z(zz) {}
+		int pomnozi() {return x * y * z;}
+		// Nemamo redefinisano saberi() ovde
+};
+
+int main() {
+	B b1, b2(1,2,3);
+	cout << b1.saberi() << b2.pomnozi();
+	return 0;
+}
+
+// Program se neće izvršiti jer ćemo dobiti upozorenje o grešci.
+
+// Metoda saberi() nije redefinisana za klasu B, a koristi se u njoj
+// Da jeste redefinisano, ispisalo bi 53
