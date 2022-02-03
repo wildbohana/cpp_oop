d891a57a60cd8d8922020c2280129fb39eda3477
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-02-03 01:23:02 +0100

    samo deklaracije

---
+++ otisak/operatori.hpp
@@ -0,0 +1,36 @@
+// BROJEVI I TO
+
+// =
+Complex& Complex::operator=(const Complex &z)
+// +=
+Complex& Complex::operator+=(const Complex &z)
+// *=
+Complex& Complex::operator*=(const Complex &z)
+
+// +
+friend Complex operator+(const Complex&, const Complex&)
+// *
+friend Complex operator*(const Complex&, const Complex&)
+// ==
+friend bool operator==(const Complex&, const Complex&)
+// <<
+friend ostream& operator<<(ostream&, const Complex&)
+
+// DINSTRING I TO
+
+// []
+char& operator[] (int);
+char operator[] (int) const;
+// =
+DinString& operator=(const DinString&);
+// +=
+DinString& operator+=(const DinString&);
+
+// ==
+friend bool operator==(const DinString&, const DinString&);
+// ==
+friend bool operator!=(const DinString&, const DinString&);
+// !=
+friend DinString operator+(const DinString&, const DinString&);
+// <<
+friend ostream& operator<<(ostream&, const DinString&);
