ee64cc4cf47969687c5322c229be1360898e0078
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-02-03 02:06:50 +0100

    pokazivač preko reference

---
+++ otisak/kodovi/o8.cpp
@@ -0,0 +1,23 @@
+// Šta će biti ispisano posle izvršavanja sledećeg koda?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	int &ra = a, &rb = b, &rc = c;	// reference
+
+	a = 1;					// a = 1
+	b = 8;					// b = 8
+	c = 9;					// c = 9
+
+	int *p = &rb;			// pravimo pokazivač na b (preko reference)
+	*p = 15;				// menjamo vrednost b iz 8 u 15
+
+	cout << a << b << c << "\n";
+	// ispisujemo staro a (1), novo b (15), i staro c (9)
+	return 0;
+}
+
+// ISPIS: 1159
