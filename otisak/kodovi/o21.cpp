79b8e2dfda9b83555487bccf8e090c0ec610adc2
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-02-03 02:21:55 +0100

    simple pointers and ++

---
+++ otisak/kodovi/o21.cpp
@@ -0,0 +1,23 @@
+// Šta će biti ispisano posle izvršavanja sledećeg koda?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 1;
+	int b = 1;
+	int *pa;
+	int *pb;
+
+	pa = &a;
+	pb = &b;
+
+	b = a++;			// b se prvo dodeli vrednost 1, pa se tek onda inkrementuje a koje će sad imati vrednost 2
+	cout << *pb;		// b = 1
+
+	cout << ++(*pa);	// prvo povećamo vrednost a na 3, pa tek onda ispišemo vrednost a
+						// a = 3
+	return 0;
+}
+
+// ISPIS: 13
